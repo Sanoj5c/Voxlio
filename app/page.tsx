779b89ffbbdee8d554022500de4867c5693d97c3
Sanoj5c <sanojdayarathna5cc@gmail.com>
2026-03-29 16:18:50 +0530

    new

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Page() {
   return (
     <div>
-      
+      <h1 className="text-2xl font-bold">Voxlio</h1>
     </div>
   )
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import Link from 'next/link' // 1. Must import Link from next/link
+import Image from 'next/image' // 2. Optional but recommended for Next.js
 
 const Navbar = () => {
   return (
-    <header className="w-full fixed z-50 bg-[var(--bg-primary)]">Navbar</header>
+    <header className="w-full fixed z-50 bg-[var(--bg-primary)]">
+      <div className='wrapper navbar-height py-4 flex justify-between items-center'>
+        {/* 3. Changed <link> to <Link> (Uppercase) */}
+        <Link href="/" className='flex gap-0.5 items-center'>
+          <img 
+            src="app/public/Assets/logo.png" 
+            alt="Voxlio"
+            width={42} 
+            height={26}
+            className="rounded-full" 
+          />
+        </Link>
+      </div>
+    </header>
   )
 }
 
